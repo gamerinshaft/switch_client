045e079c5c57931dd67f3c612183b6659c4cf6d1
j.taneichi <j.tane3ma@gmail.com>
2015-12-15 02:40:27 +0900

    リネームの実装

--- www/assets/javascript/app.js
+++ www/assets/javascript/app.js
@@ -435,6 +435,64 @@
         }
       }
     });
+    this.rename = function(group) {
+      ons.notification.confirm({
+        message: 'グループ名を変更します',
+        modifier: 'material',
+        callback: function(answer) {
+          switch(answer){
+            case 0:
+              break;
+            case 1:
+              ons.notification.prompt({
+                message: "新規グループ名を入力してください",
+                modifier: "material",
+                callback: function(name) {
+                  name = name
+                  console.log(group.id)
+                  $.ajax({
+                    url: "" + localStorage.getItem("switch-site_url") + "/api/v1/group.json",
+                    type: "PUT",
+                    data: {
+                      "group_id": parseInt(group.id),
+                      "name": name,
+                      "auth_token": localStorage.getItem("switch-auth_token")
+                    },
+                    success: function(msg){
+                      $timeout(function(){
+                        group.name = msg["response"]["group"]["name"]
+                      })
+                      ons.notification.alert({
+                        message: '' + (name||undefined) + 'に変更しました！',
+                        modifier: "material"
+                      });
+                    },
+                    error: function(error){
+                      if(error.status == 404){
+                        localStorage.setItem("switch-site_url","")
+                        app.navi.resetToPage("./404/index.html", {animation: 'lift'})
+                      }else{
+                        html = "<ul>";
+                        messages = error.responseJSON.meta.errors.forEach(function(err){
+                          html += "<li class='error'>" + err.message + "</li>";
+                        });
+                        html += "</ul>"
+                        ons.notification.alert({
+                          messageHTML: html
+                        });
+                      }
+                    }
+                  });
+                }
+              });
+            break;
+          }
+        }
+      });
+    }.bind(this)
+    this.delete = function(group) {
+      console.log(group.name);
+    }.bind(this)
     this.newIrGroup = function() {
       ons.notification.confirm({
         message: 'グループを作成します',
