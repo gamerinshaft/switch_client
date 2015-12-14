115a71f4e4895e907482bc20fcd28d4aee413966
j.taneichi <j.tane3ma@gmail.com>
2015-12-15 02:52:12 +0900

    グループの削除を実装した

--- www/assets/javascript/app.js
+++ www/assets/javascript/app.js
@@ -490,8 +490,51 @@
         }
       });
     }.bind(this)
-    this.delete = function(group) {
-      console.log(group.name);
+    this.delete = function(group,index) {
+      ons.notification.confirm({
+        message: 'グループを削除します',
+        modifier: 'material',
+        callback: function(answer) {
+          switch(answer){
+            case 0:
+              break;
+            case 1:
+              $.ajax({
+                url: "" + localStorage.getItem("switch-site_url") + "/api/v1/group.json",
+                type: "DELETE",
+                data: {
+                  "group_id": parseInt(group.id),
+                  "auth_token": localStorage.getItem("switch-auth_token")
+                },
+                success: function(msg){
+                  $timeout(function(){
+                    infraredGroupModel.groups.splice(index,1)
+                  })
+                  ons.notification.alert({
+                    message: '' + (group.name||undefined) + 'を削除しました',
+                    modifier: "material"
+                  });
+                },
+                error: function(error){
+                  if(error.status == 404){
+                    localStorage.setItem("switch-site_url","")
+                    app.navi.resetToPage("./404/index.html", {animation: 'lift'})
+                  }else{
+                    html = "<ul>";
+                    messages = error.responseJSON.meta.errors.forEach(function(err){
+                      html += "<li class='error'>" + err.message + "</li>";
+                    });
+                    html += "</ul>"
+                    ons.notification.alert({
+                      messageHTML: html
+                    });
+                  }
+                }
+              });
+            break;
+          }
+        }
+      });
     }.bind(this)
     this.newIrGroup = function() {
       ons.notification.confirm({
